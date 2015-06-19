01a224c5646faa63188bc421cddbc8d8ebff5252
Zeiger <zeigerc@USUGCOZEIGERC.merck.com>
2015-06-19 12:23:41 -0400

    added some heroku stuff

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
-CREATE TABLE IF NOT EXISTS interactions(
+CREATE TABLE IF NOT EXISTS users(
 	id      integer primary key autoincrement not null,
   name    text not null,
 	count   integer default 0 not null
  );
+CREATE TABLE IF NOT EXISTS interactions(
+  transaction_id  integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  user_id_1 integer NOT NULL,
+  user_id_2 integer NOT NULL,
+  transaction_time timestamp NOT NULL
+);
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import Flask, g, request, abort, json
 import os
 from contextlib import closing
+from datetime import datetime, date
 #app setup
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -17,7 +18,7 @@ app.config.update(dict(
 #database methods
 def connect_db():
     print 'Databse url is %s' % app.config['DATABASE']
-    return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'],detect_types=sqlite3.PARSE_DECLTYPES)
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
@@ -51,41 +52,67 @@ def query_db(query, args=(), one=False):
     get_db().commit()
     return (rv[0] if rv else None) if one else rv
 
+#helper methods
+def user_exists(username):
+    query = query_db('select * from users where name = ?',[username],one=True)
+    if query and query[1] == username:
+        return True
+    else:
+        return False
+def user_exists_id(id):
+    query = query_db('select * from users where id = ?', [int(id)])
+    if query and query[0][0] == int(id):
+        return True
+    else:
+        return False
 
 @app.route('/users/',methods=['GET','POST'])
 def user_collection():
     if request.method == 'GET':
-        return json.dumps(query_db('select * from interactions'))
+        return json.dumps(query_db('select * from users'))
     elif request.method == 'POST':
         form_data = request.form
         username = form_data['username']
-        select_result = query_db('select * from interactions where name = ?',[username])
-        if select_result and select_result[0][1] == username:
+        if user_exists(username):
             #user already exists in the database
             abort(500)
         else:
-            data_base_response = query_db('insert into interactions (name) values (?)', [username])
-            return str(query_db('select id from interactions where name=?',[username],one=True)[0])
+            data_base_response = query_db('insert into users (name) values (?)', [username])
+            return str(query_db('select id from users where name=?',[username],one=True)[0])
 
 
-@app.route('/users/<username>',methods=['GET','PUT'])
-def single_user_endpoint(username):
-    print'/users/<username> reached where username = %s' % username
-    result = query_db('select * from interactions where name = ?',[username], one=True)
+@app.route('/users/<id>',methods=['GET'])
+def single_user_endpoint(id):
+    print'/users/<id> reached where user id is = %s' % id
+    result = query_db('select * from users where id = ?',[id], one=True)
     if not result:
             abort(500)
     if request.method == 'GET':
-        if result[1] == username:
-            return str(result[2])
-        else:
-            print 'somehow the https username variable and the result from the database query don\'t match'
-            abort(500)
-    elif request.method == 'PUT':
-        query_db('update interactions set count=count+1 where name = ?',[username])
-        return '%s\'s count has been incremented' % username
-
-
+        return str(result[0])
+@app.route('/transaction/',methods=['POST'])
+def new_transaction():
+    user_id_1 = request.form['user_id_1']
+    user_id_2 = request.form['user_id_2']
+    print "Entered /transaction/ withe user_id_1=%s and user_id_2=%s" % (str(user_id_1),str(user_id_2))
+    if not user_exists_id(user_id_1) or not user_exists_id(user_id_2):
+        #either one or both of the users has not been registered
+        print 'one or more users does not exist'
+        abort(500)
+    #check to see if these users have already met
+    query1 = query_db('select * from interactions where user_id_1 = ? and user_id_2 = ?',[user_id_1,user_id_2],one=True)
+    query2 = query_db('select * from interactions where user_id_1 = ? and user_id_2 = ?',[user_id_2,user_id_1],one=True)
+    if query1 or query2:
+        #this transaction already exists
+        print 'transaction already exists'
+        abort(500)
+    #we are now free to add this new transaction
+    n_transaction = query_db('insert into interactions (user_id_1,user_id_2,transaction_time) values (?,?,?)',[user_id_1,user_id_2,datetime.now()])
+    #updating respective counts
+    query_db('update users set count=count+1 where id = ?',[user_id_1])
+    query_db('update users set count=count+1 where id = ?',[user_id_2])
+    return 'Transaction Successful'
 if not os.path.exists('./database.db'):
+    print 'initdb()'
     init_db()
 
 if __name__ == '__main__':
