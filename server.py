26b2d05aefeb5d50164543fad91b230d86321ca7
Zeiger <zeigerc@USUGCOZEIGERC.local>
2015-07-27 12:52:44 -0400

    Started working on the backend but then had to add a new endpoint for updates to be made for the. This commit contains that new endpoint plus the start of the backend

--- server.py
+++ server.py
@@ -1,19 +1,20 @@
 #all of our imports
 import sqlite3
-from flask import Flask, g, request, abort, json
+from flask import (Flask, g, request, abort, json, render_template, redirect,
+    url_for, make_response)
 import os
 from contextlib import closing
 from datetime import datetime, date
 #app setup
 app = Flask(__name__)
-app.config.from_object(__name__)
-
-
 #configuration
 app.config.update(dict(
     DATABASE = os.path.join(app.root_path, 'database.db'),
     DEBUG = True
 ))
+app.config.from_object(__name__)
+app.secret_key = '\x97u\x88\x18\xb6s\x85\x1c|\x067\xfd\x97_d\xb7ub`{\xc9\xb1z\x14'
+
 
 #database methods
 def connect_db():
@@ -66,6 +67,25 @@ def user_exists_id(id):
     else:
         return False
 
+def get_user_name(user_id):
+    query = query_db('select name from users where id=?',str(user_id),True)
+    return query[0]
+
+#webpages
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        response = make_response(url_for('index'))
+        #session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html')
+
+#API endpoints
 @app.route('/users/',methods=['GET','POST'])
 def user_collection():
     if request.method == 'GET':
@@ -80,7 +100,6 @@ def user_collection():
             data_base_response = query_db('insert into users (name) values (?)', [username])
             return str(query_db('select id from users where name=?',[username],one=True)[0])
 
-
 @app.route('/users/<id>',methods=['GET'])
 def single_user_endpoint(id):
     print'/users/<id> reached where user id is = %s' % id
@@ -89,6 +108,7 @@ def single_user_endpoint(id):
             abort(500)
     if request.method == 'GET':
         return str(result[2])
+
 @app.route('/transaction/',methods=['POST'])
 def new_transaction():
     user_id_1 = request.form['user_id_1']
@@ -111,12 +131,40 @@ def new_transaction():
     query_db('update users set count=count+1 where id = ?',[user_id_1])
     query_db('update users set count=count+1 where id = ?',[user_id_2])
     return 'Transaction Successful'
+
+@app.route('/transaction/<int:user_id>')
+def get_transaction_list(user_id):
+    if not user_exists_id(user_id):
+        print 'get_transaction_list() called with invalid user ID'
+        abort(500)
+    query1 = query_db('select * from interactions where user_id_1 = ? order by transaction_time desc', str(user_id))
+    query2 = query_db('select * from interactions where user_id_2 = ? order by transaction_time desc', str(user_id))
+    merged_list = []
+
+    while query1 and query2:
+        if query1[0][3] == query2[0][3]:
+            merged_list.append(get_user_name(query1[0][2]))
+            query1.pop(0)
+        elif query1[0][3] > query2[0][3]:
+            merged_list.append(get_user_name(query1[0][2]))
+            query1.pop(0)
+        else:
+            merged_list.append(get_user_name(query2[0][1]))
+            query2.pop(0)
+    if query1:
+        while query1:
+            merged_list.append(get_user_name(query1[0][2]))
+            query1.pop(0)
+    else:
+        while query2:
+            merged_list.append(get_user_name(query2[0][1]))
+            query2.pop(0)
+    return json.dumps(merged_list)
+
+
 if not os.path.exists('./database.db'):
     print 'initdb()'
     init_db()
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
-
-
